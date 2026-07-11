c14234ca9dd605e69ad21f75eb7668487ec52ab7
jaylordlapena817-star <jaylordlapena817@gmail.com>
2026-07-12 06:54:32 +0800

    Create history.js

---
+++ js/history.js
@@ -0,0 +1,70 @@
+import { auth, db } from "./firebase.js";
+
+import {
+onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
+import {
+ref,
+onValue
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-database.js";
+
+const betList=document.getElementById("betList");
+
+onAuthStateChanged(auth,(user)=>{
+
+if(!user){
+
+location.href="login.html";
+return;
+
+}
+
+onValue(ref(db,"bets"),(snapshot)=>{
+
+betList.innerHTML="";
+
+const data=snapshot.val();
+
+if(!data){
+
+betList.innerHTML="<p>No bets found.</p>";
+return;
+
+}
+
+Object.keys(data).reverse().forEach(key=>{
+
+const bet=data[key];
+
+if(bet.uid!==user.uid) return;
+
+betList.innerHTML+=`
+
+<div class="bet-card">
+
+<h2>${bet.draw.toUpperCase()} DRAW</h2>
+
+<p>Numbers: <b>${bet.number1} - ${bet.number2}</b></p>
+
+<p>Credits: ${bet.credits}</p>
+
+<p>Status:
+<span class="status ${bet.status.toLowerCase()}">
+${bet.status}
+</span>
+</p>
+
+<p>
+${new Date(bet.time).toLocaleString()}
+</p>
+
+</div>
+
+`;
+
+});
+
+});
+
+});
